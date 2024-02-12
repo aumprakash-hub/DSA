ce2accfc4cadc7bcbcfd2d625b3ab651fba49b9c
aumprakash-hub <aumprakash001@gmail.com>
2024-02-12 19:26:44 +0530

    Build

--- obj/Debug/net8.0/DSAConsoleApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/DSAConsoleApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSAConsoleApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9fa04f8df68fad85751acebc92b9d46d37f28d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSAConsoleApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSAConsoleApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
